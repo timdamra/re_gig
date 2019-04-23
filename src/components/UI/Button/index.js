afb84a596ac018482070cf50715ac1712104e124
Hatim Damra <hatim.damra@redbox.com>
2019-04-23 00:12:59 -0500

    added buttons

---
+++ src/components/UI/Button/index.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+import classNames from 'classnames'
+
+const styles = theme => ({
+  plainButton: {
+    border: '1px solid black',
+    maxWidth: '45%',
+    [theme.breakpoints.up('md')] : {
+      border: '1px solid white',
+      fontSize: '18px',
+      color: 'white'
+    }
+  }
+})
+
+class PlainButton extends PureComponent {
+  render = () => {
+    const {
+      buttonText = '',
+      classes
+    } = this.props
+
+    return (
+      <Button
+        variant="outlined"
+        className={classNames(classes.plainButton)}
+      >
+        {buttonText}
+      </Button>
+    )
+  }
+}
+
+export default withStyles(styles)(PlainButton)
--- src/components/UI/VideoBackground/index.js
+++ src/components/UI/VideoBackground/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 
+import PlainButton from 'components/UI/Button'
 import vid from '../../../../public/Keyboard.mp4'
 import pic from '../../../../public/pic.jpg'
 import gigPic from '../../../../public/Gigapp1024x366.png'
@@ -12,9 +13,17 @@ function VideoBackground(props) {
       <video className="gig-video" playsInline autoPlay muted loop poster={pic} id="bgvid">
         <source src={vid} type="video/webm" />
       </video>
-      <header class="gig-video__header">
+      <header className="gig-video__header">
         <img className="gig-video__logo" src={gigPic} />
         <h4>{props.subHeaderMessaging}</h4>
+        <div className="gig-video__buttons-container">
+          <PlainButton
+            buttonText="Post a Service"
+          />
+          <PlainButton
+            buttonText="Sell an Item"
+          />
+        </div>
       </header>
     </Fragment>
   )
